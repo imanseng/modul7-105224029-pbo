bd909788becb0ba51adc9b00c62beff0e6281857
imanseng <imandwisatrio99@gmail.com>
2026-05-05 15:06:12 +0700

    Latihan Soal 1

---
+++ Latihan/src/LampuPintar.java
@@ -0,0 +1,6 @@
+public class LampuPintar extends PerangkatPintar {
+    @Override
+    public void aktifkan() {
+        System.out.println("Lampu menyala dengan tingkat kecerahan standar");
+    }
+}
